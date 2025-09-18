608c862e6d0885e162c08faa567bde1506412e19
calin <chelceacalin1@gmail.com>
2025-09-19 00:00:11 +0300

    Wip reports

--- common/src/main/java/com/example/RentNow/util/EmailSenderService.java
+++ common/src/main/java/com/example/RentNow/util/EmailSenderService.java
@@ -15,63 +15,63 @@ import static com.example.RentNow.constants.EmailConstants.MAIL;
 @Slf4j
 public class EmailSenderService {
 
-	 final JavaMailSender mailSender;
-	 final String fromEmail;
-	 final Boolean isEnabled;
-	 final ExecutorService executorService;
+    final JavaMailSender mailSender;
+    final String fromEmail;
+    final Boolean isEnabled;
+    final ExecutorService executorService;
 
-	public EmailSenderService(JavaMailSender mailSender, String fromEmail, Boolean isEnabled) {
-		this.mailSender = mailSender;
-		this.fromEmail = fromEmail;
-		this.isEnabled = isEnabled;
-		this.executorService = Executors.newVirtualThreadPerTaskExecutor();
-		init();
-	}
+    public EmailSenderService(JavaMailSender mailSender, String fromEmail, Boolean isEnabled) {
+        this.mailSender = mailSender;
+        this.fromEmail = fromEmail;
+        this.isEnabled = isEnabled;
+        this.executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        init();
+    }
 
-	private void init() {
-		if (!isEnabled) {
-			log.warn(MAIL + "Running without email service");
-		} else {
-			log.info(MAIL + "Running with email service");
-		}
-	}
+    private void init() {
+        if (!isEnabled) {
+            log.warn(MAIL + "Running without email service");
+        } else {
+            log.info(MAIL + "Running with email service");
+        }
+    }
 
-	public void sendEmail(String to, String subject, String body, byte[] pdfData) {
-		if (!isEnabled) {
-			log.warn(MAIL + "Mail service not enabled");
-			return;
-		}
+    public void sendEmail(String to, String subject, String body, byte[] pdfData) {
+        if (!isEnabled) {
+            log.warn(MAIL + "Mail service not enabled");
+            return;
+        }
 
-		executorService.submit(() -> {
-			try {
-				MimeMessage mimeMessage = mailSender.createMimeMessage();
-				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-				helper.setFrom(fromEmail);
-				helper.setTo(to);
-				helper.setSubject(subject);
-				helper.setText(body, true);
-				if (pdfData != null) {
-					helper.addAttachment("report.pdf", new ByteArrayResource(pdfData));
-				}
-				mailSender.send(mimeMessage);
-				log.info(MAIL + "Mail sent successfully");
-			} catch (Exception e) {
-				log.error(MAIL + "Error sending mail {}", e.getMessage());
-			}
-		});
-	}
+        executorService.submit(() -> {
+            try {
+                MimeMessage mimeMessage = mailSender.createMimeMessage();
+                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+                helper.setFrom(fromEmail);
+                helper.setTo(to);
+                helper.setSubject(subject);
+                helper.setText(body, true);
+                if (pdfData != null) {
+                    helper.addAttachment("report.pdf", new ByteArrayResource(pdfData));
+                }
+                mailSender.send(mimeMessage);
+                log.info(MAIL + "Mail sent successfully");
+            } catch (Exception e) {
+                log.error(MAIL + "Error sending mail {}", e.getMessage());
+            }
+        });
+    }
 
-	public void shutdownExecutor() {
-		log.info(MAIL + "Shutting down the email executor service");
-		executorService.shutdown();
-		try {
-			if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
-				executorService.shutdownNow();
-			}
-		} catch (InterruptedException e) {
-			log.error(MAIL + "Error while shutting down executor service", e);
-			executorService.shutdownNow();
-			Thread.currentThread().interrupt();
-		}
-	}
+    public void shutdownExecutor() {
+        log.info(MAIL + "Shutting down the email executor service");
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            log.error(MAIL + "Error while shutting down executor service", e);
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
 }
