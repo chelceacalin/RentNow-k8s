1a2d6f0ae84d871cc61abcf4025dd7a123a7a903
calin <chelceacalin1@gmail.com>
2025-09-19 17:30:10 +0300

    WIP ingress

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 
-axios.defaults.baseURL = import.meta.env.baseURL;
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "/core";
 axios.defaults.withCredentials = true;
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App/>);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
